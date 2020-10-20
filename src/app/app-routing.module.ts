3e30c5a9c972196be353d1916933d7fe35855ccd
Giancarlo <gian-k@.live.com>
2020-10-20 03:50:02 -0300

    agregado ruteo y nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BreweryAboutComponent } from './brewery-about/brewery-about.component';
+import { BreweryBeersComponent } from './brewery-beers/brewery-beers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'beers',
+    pathMatch: 'full'  
+  },
+  {
+    path: 'beers',
+    component: BreweryBeersComponent
+  },
+  {
+    path: 'about',
+    component: BreweryAboutComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
-import { BeerCartComponent } from './beer-cart/beer-cart.component';
 
 import { FormsModule } from '@angular/forms';
+import { BreweryAboutComponent } from './brewery-about/brewery-about.component';
+import { BreweryBeersComponent } from './brewery-beers/brewery-beers.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BeerListComponent,
-    BeerCartComponent
+    BreweryAboutComponent,
+    BreweryBeersComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
